83207ddd44339fa23fa7281b7569b382e3acca3d
chris-codes01 <30329315+chris-codes01@users.noreply.github.com>
2018-05-19 17:39:18 -0500

    Submit code, test to all methods.

--- DataStructures/ArrayBased/main.cpp
+++ DataStructures/ArrayBased/main.cpp
@@ -1,15 +1,38 @@
-#include <iostream>
-using namespace std;
 #include "stack.h"
 
-int main() {
-  cout << "Hello World!\n";
-  Stack S;
-  S.push("aa");
-  S.push("aa");
-  S.push("aa");
-  S.push("aa");
-  S.push("aa");
-  cout << S.size() << endl;
+Stack :: Stack(){
+  count = 0;
+  capacity = 1000;
+}
 
+void Stack :: push(string x){
+  if(count < capacity){
+    S[count] = x;
+    count += 1;
+  }
+  else{
+    abort();
+  }
+  
+}
+
+void Stack :: pop(){
+  //Only remove when stack has items.
+  if(size() > 0){
+    count -= 1;
+  }
+}
+
+string Stack :: top(){
+  if(size() > 0){
+    //Indexed at 0
+    return S[count - 1];
+  }
+  else{
+    abort();
+  }
+}
+
+int Stack :: size(){
+  return count;
 }
