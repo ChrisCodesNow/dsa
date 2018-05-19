a3b691f9f3da512ade76bd14c1e5192329c1a2c3
chris-codes01 <30329315+chris-codes01@users.noreply.github.com>
2018-05-19 17:20:39 -0500

    Add file to test code.

---
+++ DataStructures/ArrayBased/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+#include "stack.h"
+
+int main() {
+  cout << "Hello World!\n";
+  Stack S;
+  S.push("aa");
+  S.push("aa");
+  S.push("aa");
+  S.push("aa");
+  S.push("aa");
+  cout << S.size() << endl;
+
+}
