8e01d52a15156814c733dd5a9eb35f4faf1a8fdd
chris-codes01 <30329315+chris-codes01@users.noreply.github.com>
2018-05-19 22:22:51 -0500

    Submit test code
    Basic test of functionality, not a complete test.

--- DataStructures/ArrayBased/Queue/main.cpp
+++ DataStructures/ArrayBased/Queue/main.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+using namespace std;
+#include "queue.h"
 
+int main() {
+  cout << "hello world" << endl;
+
+  Queue Q;
+  Q.push("air");
+  Q.push("boat");
+  Q.push("cattle");
+  Q.push("dog");
+  Q.push("elephant");
+  Q.push("fire");
+  Q.push("gallon");
+  Q.push("house");
+  Q.push("iron");
+
+  while (Q.size() > 0){
+    cout << "Removing " << Q.front() << endl;
+    Q.pop();
+  }
+  Q.push("Zip");
+  cout << "Removing " << Q.front() << endl;
+  Q.pop();
+}
