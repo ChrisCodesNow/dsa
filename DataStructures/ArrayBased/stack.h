2bfcdf254a820c93220ea5f91939f28c01a4f9dd
chris-codes01 <30329315+chris-codes01@users.noreply.github.com>
2018-05-19 22:27:39 -0500

    Add Big O Runtime

--- DataStructures/ArrayBased/stack.h
+++ DataStructures/ArrayBased/stack.h
@@ -7,14 +7,23 @@ using namespace std;
 class Stack{
 public:
   //constructor
+  //O(1)
   Stack();
+  
   //Insert x to stack
+  //O(1)
   void push(string x);
+  
   //remove top item from stack
+  //O(n), need to shift remaining items to the front of the Queue.
   void pop();
+  
   //get top item from stack
+  //O(1)
   string top();
+  
   //get size of the stack
+  //O(1)
   int size();
 private:
   int count;      //counts number of elements in the stack
