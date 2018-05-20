564bd47334c56f27f76e6107fc171d56b67aff07
chris-codes01 <30329315+chris-codes01@users.noreply.github.com>
2018-05-19 22:04:26 -0500

    Add code for class methods.
    Constructor Queue(), push, front(), and size()

--- DataStructures/ArrayBased/Queue/queue.cpp
+++ DataStructures/ArrayBased/Queue/queue.cpp
@@ -1,1 +1,28 @@
+#include "queue.h"
 
+Queue :: Queue(){
+  count = 0;
+  capacity = 1000;
+}
+
+void Queue :: push(string x){
+  if(size() < capacity){
+    Q[count] = x;
+    count += 1;
+  }
+  else{
+    abort();
+  }
+}
+
+string Queue :: front(){
+  if(size() > 0){
+    return Q[count - 1];
+  }
+  else{
+    abort();
+  }
+}
+int Queue :: size(){
+  return count;
+}
