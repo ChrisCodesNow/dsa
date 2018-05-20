603b242bba48f09743609b651a712f60236ab9d8
chris-codes01 <30329315+chris-codes01@users.noreply.github.com>
2018-05-19 22:02:37 -0500

    Submit Queue interface code.

--- DataStructures/ArrayBased/Queue/queue.h
+++ DataStructures/ArrayBased/Queue/queue.h
@@ -1,1 +1,26 @@
+#ifndef QUEUE_H
+#define QUEUE_H
 
+#include <string>
+using namespace std;
+
+class Queue{
+public:
+  //Default constructor
+  Queue();
+  //insert new element to back of Queue
+  void push(string x);
+  //remove front element from Queue
+  void pop();
+  //get, but do not remove, front element from Queue
+  string front();
+  //get number of elements in the Queue
+  int size();
+
+private:
+  int count;  //Count current number of elements in Queue
+  int capacity; //Maximum number allowed of elements in Queue
+  string Q[1000];   //Stack for 1,000 elements
+};
+
+#endif
