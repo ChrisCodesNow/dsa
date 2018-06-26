b25b2bd0242d5bb4953680d13c65b93bc87715aa
chris-codes01 <30329315+chris-codes01@users.noreply.github.com>
2018-06-26 11:25:56 -0500

    Create quick_sort.cpp

---
+++ DataStructures/12_MergeSort_and_QuickSort/quick_sort.cpp
@@ -0,0 +1,29 @@
+
+
+void quick_sort(int* A, int n){
+	// Implied base case: List already sorted
+	if(n < 2){
+	
+	}
+	else{
+		// Move items smaller than pivot left
+		// Move items greater than pivot right
+		// Place pivot in sorted position
+		int pivot = n -1;	// Starting pivot position
+		int separator = 0;	// Postion separating left and right subarrays
+		for(int itr = 0; itr < n - 1; itr ++){
+			if(A[itr] < A[pivot]){
+				swap(A[itr], A[separator]);
+				separator += 1;
+			}
+		}
+
+		swap(A[pivot], A[separator]);
+
+		// Recursively quick sort subarrays
+		// to the left and right of the pivot
+		quick_sort(A, separator);
+		quick_sort(&(A[separator + 1]), n - separator - 1);
+		
+	}
+}
