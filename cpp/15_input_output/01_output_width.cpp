3d6062a3c28146fba50a1df1cfe5e7a6d660ad2e
c <compuprac@gmail.com>
2019-05-07 20:23:24 -0500

    Spacing formatted

---
+++ cpp/15_input_output/01_output_width.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+
+int main(){
+
+    string l = "lowest";
+    string h = "highest";
+    string a = "average";
+
+    # read data from file
+
+    # calculate and display low, high, and average
+
+    # compute and display grade distribution
+
+
+
+
+
+
+    
+
+    int low = 59;
+    int high = 100;
+    double average = 81.8;
+
+    int space = 15;
+    cout << l << setw(space) << h << setw(space) << a << endl;
+    cout << low << setw(space) << high << setw(space) << average << endl;
+
+    return 0;
+}
