fb2ea2c0bc5f0054ded1f8f227065d2cc8522a6f
c <30329315+ChrisCodesNow@users.noreply.github.com>
2019-05-09 23:49:32 -0500

    Begin read grades
    Main logic described

---
+++ cpp/16_functions/01_prototypes.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <fstream>
+using namespace std;
+
+
+void GetDataFromFile(string, int[]);
+
+
+int main(){
+    string filename = "input.txt";
+    int grades[20];
+
+    GetDataFromFile(filename, grades);
+
+}
+
+
+void GetDataFromFile(string filename, int[] grades){
+
+}
