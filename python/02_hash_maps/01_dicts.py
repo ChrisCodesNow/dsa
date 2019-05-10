258718246ef292c30b031496371e4d92d026f347
c <30329315+ChrisCodesNow@users.noreply.github.com>
2019-05-09 23:47:06 -0500

    Filter data
    Word sentiment analysis of low scores, and sorted data

--- python/02_hash_maps/01_dicts.py
+++ python/02_hash_maps/01_dicts.py
@@ -1,10 +1,13 @@
 import urllib.request
 import random
+from collections import defaultdict, OrderedDict
+import re
 
 def get_data(url):
-    file = urllib.request.urlopen(url)
-    binary_data = file.read()
-    text = binary_data.decode(encoding="utf-8", errors="ignore")
+    response = urllib.request.urlopen(url)
+    binary_data = response.read()
+    text = binary_data.decode(encoding='utf-8', errors='ignore')
+
     return text
 
 
@@ -22,9 +25,9 @@ def get_reviews(text):
 
         reviews[url] = {'score': score, 'url': url, 'title': title, 'body': body}
 
-
     if validate_data(distinct_reviews, reviews):
         print('Data in get reviews is valid')
+
     return reviews
 
 
@@ -51,10 +54,46 @@ def subset_dict(input_dict, keys):
     for key in keys:
         subset[key] = input_dict[key]
 
-
     if validate_data(keys, subset):
-        print(" Data in subset dicts is valid")
+        print("Data in subset dicts is valid")
+
     return subset
+
+
+# Dict with scores as keys, rather than urls as keys
+def group_by_score(reviews):
+    reviews_by_score = defaultdict(list)
+    for url, review in reviews.items():
+        score = review['score']
+        review_dict = {'url': url, 'title': review['title'], 'body': review['body']}
+        reviews_by_score[score].append(review_dict)
+        
+    return reviews_by_score
+
+
+# Sentiment analysis: 
+# Frequency of words associated with low scores
+def word_sentiment_analysis(low_scores_reviews):
+    word_frequency = defaultdict(int)
+    for url in low_scores_reviews:
+        review = reviews[url]['body']
+        text = re.sub('<.*?>', '', review)              # remove HTML tags
+        words = text.strip().lower().split()    # List of words in review
+        for word in words:
+            word_frequency[word] += 1
+
+    return word_frequency
+    
+
+def sort_data(low_score_words):
+    sorted_pairs = sorted(low_score_words.items(), key=lambda tup: tup[1], reverse=True)
+    ordered_dict = OrderedDict(sorted_pairs)
+
+    # ignore top 10%, mostly stop words
+    top_ten = len(ordered_dict.keys()) // 10
+
+    # first 100 from top 90%
+    return list(ordered_dict.items())[top_ten: top_ten + 100]
 
 
 if __name__ == "__main__":
@@ -66,3 +105,8 @@ if __name__ == "__main__":
     reviews = get_reviews(text)
     low_scores_urls = get_low_scores(reviews)
     low_score_reviews = subset_dict(reviews, low_scores_urls)
+    reviews_by_score = group_by_score(reviews)
+    low_score_words = word_sentiment_analysis(low_score_reviews)
+    top_low_scores = sort_data(low_score_words)
+
+    print(top_low_scores)
