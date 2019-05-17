64e62040ac9bc4570438e152e15b67d50baea09a
c <30329315+ChrisCodesNow@users.noreply.github.com>
2019-05-16 20:46:41 -0500

    Second approach

--- python/05_dynamic_programming/02_max_path_sum.py
+++ python/05_dynamic_programming/02_max_path_sum.py
@@ -4,11 +4,34 @@ Approach 1:
     Bottom up, get largest of left and right children value to current element.
     Reduce to single number
     Edge Case: Triangle of size 1 => Single num
+
+Apprach 2: 
+    Same as approach 1, but modifies reduced_row array rather than 
+    creating a new array per iteration.
+
 Runtime: O(m n), for triangle of m rows and n columns
 Space Complexity: O(n)
+
+
+Approach 3:
+    Same as approach 1, but reduce triangle to a single number.
+    That is, input triangle is modified
+
+Runtime: O(m n), for triangle of m rows and n columns
+Space Complexity: O(1)
+
+
 '''
 class Solution():
     def max_sum_triangle(self, triangle):
+        # return self.solution_01(triangle)
+        return self.solution_02(triangle)
+        
+
+    #####################################
+    # Approach 01
+    # 
+    def solution_01(self, triangle):
         if not triangle:
             return 0
         elif len(triangle) == 1:
@@ -16,14 +39,14 @@ class Solution():
 
         reduced_row = triangle[-1][:]
         for top_row in triangle[-2: : -1]:
-            reduced_row = self.reduce_row(top_row, reduced_row)
+            reduced_row = self.reduce_row_01(top_row, reduced_row)
 
         return reduced_row[0]
 
 
     # Gets max path values from bottom to top row
     # Bottom row has 1 more element than bottom row
-    def reduce_row(self, top_row, bottom_row):
+    def reduce_row_01(self, top_row, bottom_row):
         reduced_row = []
         for j in range(len(top_row)):
             left_child = bottom_row[j]
@@ -33,6 +56,36 @@ class Solution():
 
         return reduced_row
 
+
+    #####################################
+    # Approach 02
+    # 
+    def solution_02(self, triangle):
+        if not triangle:
+            return 0
+        elif len(triangle) == 1:
+            return triangle[0]
+
+        reduced_row = triangle[-1][:]
+        for top_row in triangle[-2: : -1]:
+            self.reduce_row_02(top_row, reduced_row)
+
+        return reduced_row[0]
+
+
+    def reduce_row_02(self, top_row, bottom_row):
+        for j in range(len(top_row)):
+            left_child = bottom_row[j]
+            right_child = bottom_row[j + 1]
+            max_path_val = top_row[j] + max(left_child, right_child)
+            bottom_row[j] = max_path_val
+
+        bottom_row.pop()
+
+
+    #####################################
+    # Approach 03
+    # 
 
 # Test
 class Test:
