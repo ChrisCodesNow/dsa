9cea3f443f1e29ad1f55cac3a21c5a5bbb00a62f
c <30329315+ChrisCodesNow@users.noreply.github.com>
2019-06-11 18:40:29 -0500

    Complete toplogical sort

--- python/06_graphs/04_topological_sort.py
+++ python/06_graphs/04_topological_sort.py
@@ -1,3 +1,4 @@
+from graph import Graph
 # Use DFS to sort topologically
 
 # g is an adj list
@@ -6,6 +7,7 @@ def top_sort(g):
     g_in = in_deg(g)
     g_in_zero = [v for v in g_in if g_in[v] == 0]
 
+    print(g_in)
     visited = set(g_in_zero)
     order = []
 
@@ -25,8 +27,21 @@ def top_sort(g):
 # In degree count of input adjacency list
 def in_deg(g):
     g_in = defaultdict(int)
-    for u in g:
+    for u in range(g.vertices()):
+        g_in[u] += 0
         for v in g[u]:
             g_in[v] += 1
 
     return g_in
+
+
+if __name__ == '__main__':
+    g = Graph(6)
+    g.add_edge(5, 0)
+    g.add_edge(5, 2)
+    g.add_edge(4, 0)
+    g.add_edge(4, 1)
+    g.add_edge(2, 3)
+    g.add_edge(3, 1)
+
+    print(top_sort(g))
