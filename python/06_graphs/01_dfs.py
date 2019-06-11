29d6dd62a962800617ca045eed070673445890f3
c <30329315+ChrisCodesNow@users.noreply.github.com>
2019-06-11 16:46:58 -0500

    Complete dfs

--- python/06_graphs/01_dfs.py
+++ python/06_graphs/01_dfs.py
@@ -1,26 +1,51 @@
+from graph import Graph
 from collections import defaultdict
-class Graph:
-    def __init__(self, num_vertices):
-        self.g = defaultdict(set)
-        for i in range(num_vertices):
-            self.g[i]
+
+# Recursive depth first search
+def dfs(g, src):
+    visited = set()
+    previous = defaultdict(set)
+    previous[src] = None
+    dfs_r(g, src, visited, previous)
+    return previous
+
+
+def dfs_r(g, src, visited, previous):
+    visited.add(src)
+    for v in g[src]:
+        if v not in visited:
+            previous[v] = src
+            dfs_r(g, v, visited, previous)
+
+        
+def build_path(src, dest, previous):
+    if dest not in previous:
+        return []
     
-    def add_edge(self, u, v):
-        self.g[u].add(v)
+    return build_path(src, previous[dest], previous) + [dest]
 
-    def remove_edge(self, u, v):
-        if u in self.g.keys():
-            if v in self.g[u]:
-                g[u].remove(v)
 
-    def print(self):
-        for u in self.g:
-            print(f'{u} -> {self.g[u]}')
+
+
+def are_connected(g, src, dest):
+    previous = dfs(g, src)
+    path = build_path(src, dest, previous)
+
+    print(f'{src} to {dest} : {path}')
 
 if __name__ == "__main__":
-    g = Graph(3)
+    g = Graph(6)
     g.add_edge(0, 1)
     g.add_edge(0, 2)
-    g.add_edge(1, 2)
+    g.add_edge(0, 6)
+    g.add_edge(1, 3)
+    g.add_edge(1, 4)
+    g.add_edge(2, 3)
+    g.add_edge(3, 4)
+    g.add_edge(3, 5)
+    # g.add_edge(5, 6)
+    g.add_edge(6, 2)
 
     g.print()
+    are_connected(g, 0, 5)
+    are_connected(g, 1, 6)
