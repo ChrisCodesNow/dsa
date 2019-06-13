505509bae1a7a6a021325b291a3ec189bb03153e
c <30329315+ChrisCodesNow@users.noreply.github.com>
2019-06-13 10:39:15 -0500

    Edit, add test case

--- python/06_graphs/weighted_graph.py
+++ python/06_graphs/weighted_graph.py
@@ -1,15 +1,5 @@
 from collections import defaultdict
 
-# class Edge:
-#     def __init__(self, u, v, weight):
-#         self.u = u
-#         self.v = v
-#         self.weight = weight
-
-#     def __str__(self):
-#         return f'Edge: {self.u} - {self.v} \t weight = {self.weight}'
-
-
 class Graph:
     def __init__(self, num_vertices):
         self.num_vertices = num_vertices
@@ -30,9 +20,28 @@ class Graph:
         for u in self.g:
             print(f'{u} -> {self.g[u]}')
 
+        print('Weights:')
+        for u in self.g:
+            for v in self.g[u]:
+                print(f'Edge {u} -- {v} \t weight = {self.weights[u, v]}')
+
 
     def __getitem__(self, u):
         return self.g[u]
 
     def vertices(self):
         return self.num_vertices
+
+
+if __name__ == '__main__':
+    g = Graph(5)
+
+    g.add_edge(0, 1, 1)
+    g.add_edge(3, 4, 2)
+    g.add_edge(1, 4, 3)
+    g.add_edge(1, 3, 4)
+    g.add_edge(1, 2, 5)
+    g.add_edge(2, 4, 6)
+    g.add_edge(0, 2, 7)
+
+    g.print()
